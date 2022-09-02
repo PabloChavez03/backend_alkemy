4eb70af92f871bb6b029265a0a497897477be487
PabloChavez03 <pablooscarchavez@gmail.com>
2022-09-01 21:04:31 -0300

    FIX(models/User): change name in db

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-  sequelize.define('Operation', {
+  sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
